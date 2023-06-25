768c02d4c29921bd603c5903ebe3f79f87cbed4e
ayurrin <yuriamakawa73@gmail.com>
2023-06-25 20:36:09 +0900

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import openai
 import secret_keys  # 外部ファイルにAPI keyを保存
 
-openai.api_key = secret_keys.openai_api_key
+openai.api_key = secret_keys.OPEN_AI_API.openai_api_key
 
 system_prompt = """
 あなたは優秀なプログラミング講師です。
